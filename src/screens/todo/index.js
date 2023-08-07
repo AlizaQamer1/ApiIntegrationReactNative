a8651946651a38c8b7509274de86032b5f402ad8
AlizaQamer1 <aliza.qamar@citrusbits.com>
2023-08-07 15:54:50 +0500

    it will add changes in todo file

--- src/screens/todo/index.js
+++ src/screens/todo/index.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, TextInput, Button, Switch } from 'react-native';
 import axios from 'axios';
@@ -7,101 +6,97 @@ import { storage } from '../../Storage';
 import { todo } from '../../helpers/GetApi';
 import styles from './Style';
 
-const BASE_URL = 'https://dummyjson.com';
+
 
 export default function Todo() {
-    const [todos, setTodos] = useState([]);
-    const [newTodo, setNewTodo] = useState('');
-  
-    useEffect(() => {
-      fetchAndCombineTodos();
-    }, []);
-  
-    const fetchAndCombineTodos = async () => {
-      try {
-        const id = await storage.getString('id');
-        if (!id) {
-          setTodos([]);
-          return;
-        }
-    
-        const storedTodos = storage.getString(`todos_${id}`);
-        const storedTodosArray = storedTodos ? JSON.parse(storedTodos) : [];
-    
-        const apiData = await todo();
-        const apiTodos = apiData.todos;
-    
-        const combinedTodosMap = new Map();
-    
-        storedTodosArray.forEach((todo) => combinedTodosMap.set(todo.id, todo));
-    
-        // Initialize the completed state of each API todo using stored value or false if not found
-        apiTodos.forEach((todo) => {
-          const storedTodo = combinedTodosMap.get(todo.id);
-          const completed = storedTodo ? storedTodo.completed : false;
-          combinedTodosMap.set(todo.id, { ...todo, completed });
-        });
-    
-        const combinedTodos = Array.from(combinedTodosMap.values());
-    
-        setTodos(combinedTodos);
-      } catch (error) {
-        console.error('Error fetching and combining Todos:', error);
-      }
-    };
-    
-    const toggleTodoCompleted = async (id) => {
-      const updatedTodos = todos.map((todo) => {
-        if (todo.id === id) {
-          const updatedTodo = { ...todo, completed: !todo.completed };
-          // Update storage with the new completed state
-          updateTodoInStorage(updatedTodo);
-          return updatedTodo;
-        }
-        return todo;
-      });
-    
-      setTodos(updatedTodos);
-    };
-    
-    const updateTodoInStorage = async (updatedTodo) => {
-      try {
-        const userId = await storage.getString('id');
-        const storedTodos = storage.getString(`todos_${userId}`);
-        if (storedTodos) {
-          const parsedTodos = JSON.parse(storedTodos);
-          const updatedTodos = parsedTodos.map((todo) =>
-            todo.id === updatedTodo.id ? updatedTodo : todo
-          );
-          storage.set(`todos_${userId}`, JSON.stringify(updatedTodos));
-        }
-      } catch (error) {
-        console.error('Error updating todo in storage:', error);
-      }
-    };
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-  
-  
+  useEffect(() => {
+    fetchAndCombineTodos();
+  }, []);
+
+  const fetchAndCombineTodos = async () => {
+    try {
+      const id = await storage.getString('id');
+      if (!id) {
+        setTodos([]);
+        return;
+      }
+
+      const storedTodos = storage.getString(`todos_${id}`);
+      const storedTodosArray = storedTodos ? JSON.parse(storedTodos) : [];
+
+      const apiData = await todo();
+      const apiTodos = apiData.todos;
+
+      const combinedTodosMap = new Map();
+
+      storedTodosArray.forEach((todo) => combinedTodosMap.set(todo.id, todo));
+
+      // Initialize the completed state of each API todo using stored value or false if not found
+      apiTodos.forEach((todo) => {
+        const storedTodo = combinedTodosMap.get(todo.id);
+        const completed = storedTodo ? storedTodo.completed : false; // Initialize with the stored completion state or false
+        combinedTodosMap.set(todo.id, { ...todo, completed });
+      });
+
+      const combinedTodos = Array.from(combinedTodosMap.values());
+
+      setTodos(combinedTodos);
+    } catch (error) {
+      console.error('Error fetching and combining Todos:', error);
+    }
+  };
+
+  const toggleTodoCompleted = async (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        const updatedTodo = { ...todo, completed: !todo.completed };
+
+        updateTodoInStorage(updatedTodo);
+        return updatedTodo;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
+
+  const updateTodoInStorage = async (updatedTodo) => {
+    try {
+      const userId = await storage.getString('id');
+      const storedTodos = storage.getString(`todos_${userId}`);
+      if (storedTodos) {
+        const parsedTodos = JSON.parse(storedTodos);
+        const updatedTodos = parsedTodos.map((todo) =>
+          todo.id === updatedTodo.id ? updatedTodo : todo
+        );
+        storage.set(`todos_${userId}`, JSON.stringify(updatedTodos));
+      }
+    } catch (error) {
+      console.error('Error updating todo in storage:', error);
+    }
+  };
 
   const handleAddTodo = async () => {
     if (newTodo.trim() !== '') {
       const newTodoItem = {
         id: Date.now().toString(),
         todo: newTodo,
-        completed: false, // Initialize completed state as false
+        completed: false,
       };
-  
+
       setTodos((prevTodos) => [...prevTodos, newTodoItem]);
-  
+
       const userId = await storage.getString('id');
       const storedTodos = storage.getString(`todos_${userId}`);
       const updatedTodos = storedTodos ? [...JSON.parse(storedTodos), newTodoItem] : [newTodoItem];
       storage.set(`todos_${userId}`, JSON.stringify(updatedTodos));
-  
+
       setNewTodo('');
     }
   };
-  
 
   return (
     <View style={styles.container}>
